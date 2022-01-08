c9100b6718d3a638e0ca2b526555d2bcf7dd01dd
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-08 02:04:55 -0500

    Update and rename angryProfessor.go to angry_Professor.go

---
+++ angry_Professor.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt"
+import "strconv"
+import "strings"
+import "os"
+import "bufio"
+
+
+func check_tardiness(s []int32, k int, n int) bool {
+        
+  
+  
+    return True
