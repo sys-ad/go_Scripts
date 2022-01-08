9cf3fdfbdad55635fae195dd2c9a8b75419ee4ab
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-08 15:10:05 -0500

    Update angry_Professor.go

--- angry_Professor.go
+++ angry_Professor.go
@@ -16,11 +16,13 @@ func main() {
     in := bufio.NewReader(os.Stdin)    
     var t, n, k int // t = number of test cases, n = number of students (size of array input array length), k = threshold
     var count := 0
-    var a [n]int
+    var a []uint64
+    a = make([]uint64, n)
+    
     fmt.Scan(&t, &n, &k)
     
     for i := 0; i <= n; i++ {
-        fmt.Scan(&i)
+        fmt.Scan(&a[i])
         if a[i] <= 0 {
             count++
         }else {
