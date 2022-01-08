d1d4a19ea084e1226552b5e1277ad288c26c04ec
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-08 08:00:59 -0500

    Update angry_Professor.go

--- angry_Professor.go
+++ angry_Professor.go
@@ -14,8 +14,9 @@ return
 func main() {
     
     in := bufio.NewReader(os.Stdin)    
-    var i, k int
-    var 
-                
-        
+    var t, n, k int // t = number of test cases, n = number of students (size of array input array length), k = threshold
+    var a [n]int
+    fmt.Scan(&t, &n, &k)
+    
+    for 
               
