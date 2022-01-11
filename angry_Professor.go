0b87760ce94bef368580ac0078e1c88793a4cae4
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-10 22:10:32 -0500

    Update angry_Professor.go

--- angry_Professor.go
+++ angry_Professor.go
@@ -6,24 +6,14 @@ import "fmt"
         
 func main() {
     
-    var t, n, k int // t = number of test cases, n = number of students (size of array input array length), k = threshold
-    var count = 0
-    var a []uint64
-    
-    fmt.Scan(&t, &n, &k)
-    a = make([]uint64, n)
-        
-    for i := 0; i <= n; i++ {
-        fmt.Scanf("%d", &a[i])
-         if a[i] <= 0 {
-             count++
-         }else {
-                continue
-         }
-    }
-    if count <= k {
-        fmt.Println("YES\n")
-    }else {
-        fmt.Println("NO\n")
-    }
+   
 }
+
+func readData() ([]int, error) {
+        var length int
+        
+        _, err := fmt.Scanf("%d", &length)
+        if err != nil {
+                log.Fatal(err)
+        }
+        
