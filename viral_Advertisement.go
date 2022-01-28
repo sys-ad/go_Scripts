e49e101e73a8fb5536f0ece026928721e86ad9a3
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-28 00:13:18 -0500

    Update viral_Advertisement.go

--- viral_Advertisement.go
+++ viral_Advertisement.go
@@ -22,10 +22,12 @@ func viralAdvertising(n int32) int32 {
         return 2 
     }
     for i := 0; i <= days; i++ {
-        shared := 0
-        shared == (n % 2) x
-        something divisible by 2, then add that to existing count
+        var shared int32
+        shared := (n % 2) * 3
         
+        something divisible by 2, then add that to existing count
+        liked is equal to last count x 3 or shared
+        cumulative is the number for succesive days 
     i++
     }
     
